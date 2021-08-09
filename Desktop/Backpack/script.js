73cda7f40bf822a7ea3754c18ea2afd2801fd4c7
Dworn1k <michal.dwornikowski@gmail.com>
2021-08-09 15:29:33 +0200

    zis is not werking

--- Desktop/Backpack/script.js
+++ Desktop/Backpack/script.js
@@ -1,7 +1,8 @@
 
 document.getElementById("btn").addEventListener('click', write);
+var item = document.getElementById("item").value;
 
-function write (){
+/*function write (){
     var item = document.getElementById("item").value;
     if (item=="") return;
     else {
@@ -11,6 +12,28 @@ function write (){
     document.getElementById("myList").appendChild(node);
     document.getElementById("item").value=null;
     }
+} */
+
+function addElement() {
+    if (item=="") return;
+    else if (item.length<=3) addToSecond();
+    else addToFirst;
+}
+
+function addToFirst(){
+    var node = document.createElement("LI");
+    var textnode = document.createTextNode(item);
+    node.appendChild(textnode);
+    document.getElementById("myList").appendChild(node);
+    document.getElementById("item").value=null;
+}
+
+function addToSecond(){
+    var node=document.createElement("div");
+    var textnode=document.createTextNode(item);
+    node.appendChild(textnode);
+    document.getElementById("list2").appendChild(node);
+    document.getElementById("item").value=null;
 }
 
 
