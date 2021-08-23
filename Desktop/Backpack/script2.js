e08d26d0a7dc5298f94c75b39c0745f7a391be8c
Dworn1k <79044666+Dworn1k@users.noreply.github.com>
2021-08-23 14:12:39 +0200

    Add files via upload

--- Desktop/Backpack/script2.js
+++ Desktop/Backpack/script2.js
@@ -1,12 +1,12 @@
 document.getElementById("btn").addEventListener('click',()=>{
     var item=document.getElementById("item").value;
     if(item==="")return;
-    else if(item.length>3) createElement("li","list1");
-    else createElement("div","list2");
+    else createElement("li","list1");
 })
 
 var i=0;
-var j=0;
+let elements=[];
+let checkedElements=[];
 
 function createElement(tag,parent){
     var node=document.createElement(tag);
@@ -14,34 +14,40 @@ function createElement(tag,parent){
     node.appendChild(textnode);
     document.getElementById(parent).appendChild(node);
 
-    if(tag==="li"){
-        node.id=tag+i;
-        i++;
-    }else {
-        node.id=tag+j;
-        j++;
-    }
+    node.id=tag+i;
+    i++;
+    
+    elements.push(node);
+
     createBox(node,tag);
     createButton(node,tag);
     document.getElementById("item").value=null;
-    percentageDisplay(1,0);
+    percentageDisplay();
 }
 
 function createBox(node,tag){
     var box=document.createElement("input");
     box.type="checkbox";
     box.className="checkboxes";
-    if (tag==="li"){
-        box.id=tag+"Box"+i;
-    }else box.id=tag+"Box"+j;
     node.appendChild(box);
+    box.id=tag+"Box"+i;
+    
 
     box.addEventListener('change',()=>{
         if(box.checked){
             document.getElementById(node.id).style.textDecoration="line-through";
+            checkedElements.push(box.parentNode);
+            percentageDisplay();
         }
         else{
             document.getElementById(node.id).style.textDecoration="none";
+            for(let i=0; i<checkedElements.length;i++){
+                if (box.parentNode===checkedElements[i]){
+                    checkedElements.splice(i,1);
+                    i--;
+                }
+            }
+            percentageDisplay();
         }
     })
 }
@@ -51,29 +57,42 @@ function createButton(node,tag){
     deleteButton.type="button";
     deleteButton.className="deleteButtons";
     deleteButton.innerHTML="Delete";
-    if (tag==="li"){
-        deleteButton.id=tag+"DeleteButton"+i;
-    }else deleteButton.id=tag+"DeleteButton"+j;
+    
+    deleteButton.id=tag+"DeleteButton"+i;
+   
     node.appendChild(deleteButton);
 
     deleteButton.addEventListener('click',() =>{
+        for(let i=0;i<elements.length;i++){
+            if(deleteButton.parentNode===elements[i]){
+                if (deleteButton.previousSibling.checked){
+                    for(let j=0; j<checkedElements.length;j++){
+                        if (deleteButton.parentNode===checkedElements[j]){
+                            checkedElements.splice(j,1);
+                            j--;
+                        }
+                    }
+                }
+
+                elements.splice(i,1);
+                i--;
+            }
+        }
         var itemToRemove=document.getElementById(node.id);
         itemToRemove.remove(node);
-        
-        // Problem - potrzebuję wiedzieć, czy box w pozycji. którą
-        // usuwam jest zaznaczony czy nie żeby przekazać odpowiednie argumenty do funkcji
-        if()percentageDisplay(-1,-1);
-        else percentageDisplay(-1,0);
+        percentageDisplay();
     })
+    
 }
-var items=0;
-var checked=0;
-function percentageDisplay(howManyItems,howManyChecked){
-    var percentagePacked=0;
-    items+=howManyItems;
-    checked+=howManyChecked;
-    percentagePacked=(checked*100)/items;
+
+function percentageDisplay(){
+    if (elements.length===0){
+        document.getElementById("percentage").innerHTML="0% PACKED";
+        return;
+    }
+    let percentagePacked=(checkedElements.length*100)/elements.length;
     document.getElementById("percentage").innerHTML=Math.round(percentagePacked)+"% PACKED";
+    document.getElementById("progressBar").style.width=percentagePacked+"%";
 }
 
 
