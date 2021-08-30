d5d72b33d6f3766f6dd42447370617374e5f5ec6
Dworn1k <79044666+Dworn1k@users.noreply.github.com>
2021-08-30 11:10:36 +0200

    Add files via upload

--- Desktop/Backpack/script.js
+++ Desktop/Backpack/script.js
@@ -1,4 +1,3 @@
-
 //document.getElementById("btn").addEventListener('click', addElement);
 
 
@@ -20,7 +19,6 @@
     else if (item.length<=3) addToSecond();
     else addToFirst();
 }
-
 function addToFirst(){
     var node = document.createElement("LI");
     var textnode = document.createTextNode(item.value);
@@ -28,7 +26,6 @@ function addToFirst(){
     document.getElementById("myList").appendChild(node);
     document.getElementById("item").value=null;
 }
-
 function addToSecond(){
     var node=document.createElement("div");
     var textnode=document.createTextNode(item.value);
@@ -58,6 +55,13 @@ function createElement(tag,parent){
     var textnode=document.createTextNode(item.value);
     node.appendChild(textnode);
     document.getElementById(parent).appendChild(node);
+
+    let deleteButton=document.createElement("button");
+    deleteButton.type="button";
+    deleteButton.className="deleteButtons";
+    deleteButton.innerHTML="Delete";
+    node.appendChild(deleteButton);
+
     
     let box=document.createElement("input");
     box.type="checkbox";
@@ -84,15 +88,29 @@ function createElement(tag,parent){
             howManyChecked++;
             console.log("howManyChecked="+howManyChecked);
             percentagePacked=(howManyChecked*100)/howManyItems;
-            document.getElementById("percentage").innerHTML=Math.round(percentagePacked)+"% PACKED"
+            document.getElementById("percentage").innerHTML=Math.round(percentagePacked)+"% PACKED";
         }
         else{
             document.getElementById(node.id).style.textDecoration="none";
             howManyChecked--;
             console.log("howManyChecked="+howManyChecked);
             percentagePacked=(howManyChecked*100)/howManyItems;
-            document.getElementById("percentage").innerHTML=Math.round(percentagePacked)+"% PACKED"
+            document.getElementById("percentage").innerHTML=Math.round(percentagePacked)+"% PACKED";
         }
+    })
+
+    deleteButton.addEventListener('click',() =>{
+        var itemToRemove=document.getElementById(node.id);
+        howManyItems--;
+        if (box.checked=true){
+            howManyChecked--;
+        }
+        itemToRemove.remove(node);
+        percentagePacked=(howManyChecked*100)/howManyItems;
+        document.getElementById("percentage").innerHTML=Math.round(percentagePacked)+"% PACKED";
+        console.log("howManyItems: "+howManyItems);
+        console.log("howManyChecked: "+howManyChecked);
+        console.log("Percentage Packed: "+percentagePacked);
     })
 
     document.getElementById("item").value=null;
--- Desktop/Backpack/script2.js
+++ Desktop/Backpack/script2.js
@@ -11,38 +11,48 @@ let checkedElements=[];
 function createElement(tag,parent){
     var node=document.createElement(tag);
     var textnode=document.createTextNode(item.value);
-    node.appendChild(textnode);
+    var txtSpan=document.createElement("span");
+    txtSpan.appendChild(textnode);
+    node.appendChild(txtSpan);
     document.getElementById(parent).appendChild(node);
 
+    txtSpan.id="span"+tag+i;
+    txtSpan.className="liSpan";
+
+    var liDiv=document.createElement("div");
+    liDiv.id="div"+tag+i;
+    liDiv.className="liDiv";
+    node.appendChild(liDiv);
+
     node.id=tag+i;
-    i++;
+    //i++;
     
     elements.push(node);
 
-    createBox(node,tag);
-    createButton(node,tag);
+    createBox(node,tag,liDiv);
+    createButton(node,tag,liDiv);
     document.getElementById("item").value=null;
     percentageDisplay();
 }
 
-function createBox(node,tag){
+function createBox(node,tag,liDiv){
     var box=document.createElement("input");
     box.type="checkbox";
     box.className="checkboxes";
-    node.appendChild(box);
+    liDiv.appendChild(box);
     box.id=tag+"Box"+i;
     
 
     box.addEventListener('change',()=>{
         if(box.checked){
             document.getElementById(node.id).style.textDecoration="line-through";
-            checkedElements.push(box.parentNode);
+            checkedElements.push(liDiv.parentNode);
             percentageDisplay();
         }
         else{
             document.getElementById(node.id).style.textDecoration="none";
             for(let i=0; i<checkedElements.length;i++){
-                if (box.parentNode===checkedElements[i]){
+                if (liDiv.parentNode===checkedElements[i]){
                     checkedElements.splice(i,1);
                     i--;
                 }
@@ -52,22 +62,23 @@ function createBox(node,tag){
     })
 }
 
-function createButton(node,tag){
+function createButton(node,tag,liDiv){
     let deleteButton=document.createElement("button");
     deleteButton.type="button";
     deleteButton.className="deleteButtons";
     deleteButton.innerHTML="Delete";
     
     deleteButton.id=tag+"DeleteButton"+i;
+    i++;
    
-    node.appendChild(deleteButton);
+    liDiv.appendChild(deleteButton);
 
     deleteButton.addEventListener('click',() =>{
         for(let i=0;i<elements.length;i++){
-            if(deleteButton.parentNode===elements[i]){
+            if(liDiv.parentNode===elements[i]){
                 if (deleteButton.previousSibling.checked){
                     for(let j=0; j<checkedElements.length;j++){
-                        if (deleteButton.parentNode===checkedElements[j]){
+                        if (liDiv.parentNode===checkedElements[j]){
                             checkedElements.splice(j,1);
                             j--;
                         }
@@ -87,12 +98,11 @@ function createButton(node,tag){
 
 function percentageDisplay(){
     if (elements.length===0){
-        document.getElementById("percentage").innerHTML="0% PACKED";
+        document.getElementById("percentageTxt").innerHTML="0% PACKED";
         return;
     }
     let percentagePacked=(checkedElements.length*100)/elements.length;
-    document.getElementById("percentage").innerHTML=Math.round(percentagePacked)+"% PACKED";
-    document.getElementById("progressBar").style.width=percentagePacked+"%";
+    let percentage=Math.round(percentagePacked);
+    document.getElementById("percentageTxt").innerHTML=percentage+"% PACKED";
+    document.getElementById("progressBar").value=percentage;
 }
-
-
